75fb266ee1c70666c494b1ff85ef5a23b03a123b
Pali Ondras <pali.ondras@gmail.com>
2013-11-21 10:14:44 +0100

    Update GoogleMapController.php
    fixing center point

--- code/extension/GoogleMapController.php
+++ code/extension/GoogleMapController.php
@@ -8,7 +8,6 @@ class GoogleMapController extends Extension {
     
     public function onBeforeInit() {
         Requirements::themedCSS('GoogleMaps', 'googlemaps');
-        
         if(
             $this->owner->Maps()->exists()
             && $this->owner->ShowMap
@@ -22,29 +21,114 @@ class GoogleMapController extends Extension {
             Requirements::javascript('googlemaps/javascript/gmap3.min.js');
         }
     }
-    
+ 	   
     public function onAfterInit() {
         if(
             $this->owner->Maps()->exists()
             && $this->owner->ShowMap
             && !$this->owner->StaticMap
         ) {
-            foreach($this->owner->Maps() as $map) {
-                $vars = array(
-                    'MapID'         => "google-map-dynamic-{$map->ID}",
-                    'Content'       => $map->Content,
-                    'Address'       => ($map->Address) ? str_replace('/n', ',', $map->Address) . ',' . $map->PostCode : 'false',
-                    'Latitude'      => ($map->Latitude) ? $map->Latitude : 'false',
-                    'Longitude'     => ($map->Longitude) ? $map->Longitude : 'false',
-                    'Zoom'          => $map->ZoomLevel
-                );
-            
-                Requirements::javascriptTemplate(
-                    'googlemaps/javascript/GoogleMap.js',
-                    $vars
-                );
-            }
-        }
+        	if ($this->owner->OnlyOneMap == false) {
+				foreach($this->owner->Maps() as $map) {
+	                $vars = array(
+	                    'MapID'         => "google-map-dynamic-{$map->ID}",
+	                    'Content'       => $map->Content,
+	                    'Address'       => ($map->Address) ? str_replace('/n', ',', $map->Address) . ',' . $map->PostCode : 'false',
+	                    'Latitude'      => ($map->Latitude) ? $map->Latitude : 'false',
+	                    'Longitude'     => ($map->Longitude) ? $map->Longitude : 'false',
+	                    'Zoom'          => $map->ZoomLevel
+	                );
+	            	
+                	Requirements::javascriptTemplate(
+	                    'googlemaps/javascript/GoogleMap.js',
+                    	$vars
+	                );
+				}
+			}
+			else {
+				$centerAdress = "";
+				$centerLatitude;
+				$centerLongitude;
+				$markerValues = "";
+				$zoom = 10;
+				$mapID = "";
+				$autofit = ($this->owner->AutoFit) ? ",autofit: {}" : "";
+				for ($i = 0; $i<count($this->owner->Maps()); $i++) {
+					$map = $this->owner->Maps();
+					$map = $map[$i];
+	            	if ($i == 0) {
+	        			$centerAdress = ($map->Address) ? "address: '".str_replace('/n', ',', $map->Address) . ',' . $map->PostCode ."'," : '';
+						$zoom = $map->ZoomLevel;
+						$mapID = "google-map-dynamic-{$map->ID}";
+						$centerLatitude = ($map->Latitude) ? $map->Latitude : 'false';
+						$centerLongitude = ($map->Longitude) ? $map->Longitude : 'false';
+	            	}
+					$address = ($map->Address) ? str_replace('/n', ',', $map->Address) . ',' . $map->PostCode : 'false';
+					$markerValues .= "{";						
+						if ($address != 'false') {
+							$markerValues .= 'address:"'.$address.'",';
+						} else {
+						$markerValues .= "latLng:[";
+							$markerValues .= ($map->Latitude) ? $map->Latitude : 'false';
+							$markerValues .= ",";
+							$markerValues .= ($map->Longitude) ? $map->Longitude : 'false';
+			 			$markerValues .= "],";
+						}
+						$markerValues .= "data: '".$map->Content."',";
+						$markerValues .= "options:{icon: 'http://maps.google.com/mapfiles/marker_yellow.png'}";
+	                $markerValues .= "}";
+					if ($i != count($this->owner->Maps())-1)
+						$markerValues .= ",";
+	            }
+
+				$map = "";
+				
+				if ($autofit == "") {
+					$map = '
+							map:{
+					            '.$centerAdress.'
+					            options:{
+					              center:['.$centerLatitude.','.$centerLongitude.'],
+					              zoom: '.$zoom.'
+					            }
+					        },
+					';
+				}
+				
+				Requirements::customScript('
+					jQuery(document).ready(function() {
+					    jQuery(".'.$mapID.'").gmap3({
+					    	'.$map.'
+					        marker:{
+					        	values:[
+					            '.$markerValues.'
+					            ],
+					            options:{
+					              draggable: false
+					            },
+					            events:{
+					              click: function(marker, event, context){
+					                var map = $(this).gmap3("get"),
+					                  infowindow = $(this).gmap3({get:{name:"infowindow"}});
+					                if (infowindow){
+			                  		  infowindow.open(map, marker);
+					                  infowindow.setContent(context.data);
+					                } else {
+					                  $(this).gmap3({
+					                    infowindow:{
+					                      anchor:marker, 
+					                      options:{content: context.data}
+					                    }
+					                  });
+					                }
+					              }
+					    		}
+					        }'.$autofit.'
+					    });
+					});
+				');		
+			}
+		}
     }
 
     public function GoogleMaps() {
