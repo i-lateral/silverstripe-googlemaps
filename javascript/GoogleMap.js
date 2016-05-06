de5592ef6b40e5165eaf73df0faefc2889b5f2ab
Florian Thoma <florian.thoma@innoweb.com.au>
2016-05-06 15:57:20 +1000

    center map on lnglat not on address since address might not be found

--- javascript/GoogleMap.js
+++ javascript/GoogleMap.js
@@ -1,7 +1,6 @@
 jQuery(document).ready(function() {
     jQuery('.$MapID').gmap3({
         map:{
-            address: "$Address",
             options: {
                 center: [$Latitude,$Longitude],
                 zoom: $Zoom
