d17321ea3b90d7a4b03d2af8529266381cd6efca
Florian Thoma <florian.thoma@innoweb.com.au>
2016-05-09 11:13:17 +1000

    fix loading of js if framework version of jquery gets overwritten by custom jquery version

--- code/extension/GoogleMapController.php
+++ code/extension/GoogleMapController.php
@@ -7,24 +7,6 @@
 class GoogleMapController extends Extension
 {
     
-    public function onBeforeInit()
-    {
-        Requirements::themedCSS('GoogleMaps', 'googlemaps');
-        
-        if (
-            $this->owner->Maps()->exists()
-            && $this->owner->ShowMap
-            && !$this->owner->StaticMap
-        ) {
-            $config = SiteConfig::current_site_config();
-            $key = ($config->APIKey) ? "&key={$config->APIKey}" : '';
-            
-            Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
-            Requirements::javascript("http://maps.googleapis.com/maps/api/js?sensor=false" . $key);
-            Requirements::javascript('googlemaps/javascript/gmap3.min.js');
-        }
-    }
-    
     public function onAfterInit()
     {
         if (
@@ -32,6 +14,14 @@ class GoogleMapController extends Extension
             && $this->owner->ShowMap
             && !$this->owner->StaticMap
         ) {
+            // load js requirements
+            $config = SiteConfig::current_site_config();
+            $key = ($config->APIKey) ? "&key={$config->APIKey}" : '';
+            Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
+            Requirements::javascript("http://maps.googleapis.com/maps/api/js?sensor=false" . $key);
+            Requirements::javascript('googlemaps/javascript/gmap3.min.js');
+            
+            // load maps
             foreach ($this->owner->Maps() as $map) {
                 $vars = array(
                     'MapID'         => "google-map-dynamic-{$map->ID}",
@@ -39,7 +29,7 @@ class GoogleMapController extends Extension
                     'Address'       => ($map->Address) ? str_replace('/n', ',', $map->Address) . ',' . $map->PostCode : 'false',
                     'Latitude'      => ($map->Latitude) ? $map->Latitude : 'false',
                     'Longitude'     => ($map->Longitude) ? $map->Longitude : 'false',
-                    'Zoom'          => $map->ZoomLevel
+                    'Zoom'          => $map->Zoom
                 );
             
                 Requirements::javascriptTemplate(
